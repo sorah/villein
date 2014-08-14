cdd796736d569193c9fd336ef51c2b318c722b9f
Shota Fukumori (sora_h) <sorah@cookpad.com>
2014-08-14 11:01:48 +0900

    Refactor native event-handler

--- src/villein-event-handler.c
+++ src/villein-event-handler.c
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #include <fcntl.h>
 #include <sys/select.h>
@@ -16,7 +17,142 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
+#define BUFSIZE 2048
+
+typedef struct {
+  const char *hostname;
+  const char *port;
+  struct addrinfo *host_head;
+  int sock;
+} Mission;
+
 extern char **environ;
+
+void
+cleanup(Mission *mission)
+{
+  if (mission->host_head != NULL)
+    freeaddrinfo(mission->host_head);
+  if (mission->sock != -1)
+    close(mission->sock);
+}
+
+int
+is_query_p()
+{
+  return getenv("SERF_EVENT") != NULL && strcmp(getenv("SERF_EVENT"), "query") == 0;
+}
+
+void
+enable_nonblock(Mission *mission, int fd)
+{
+  errno = 0;
+  unsigned int flags = fcntl(fd, F_GETFL);
+
+  if (errno != 0) {
+    perror("oops: F_GETCL");
+    exit(1);
+  }
+
+  if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+    perror("oops: F_SETCL");
+    exit(1);
+  }
+}
+
+void
+connect_villein(Mission *mission)
+{
+  int err;
+  struct addrinfo hints, *host;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_family = PF_UNSPEC;
+
+  if ((err = getaddrinfo(mission->hostname, mission->port, &hints, &mission->host_head)) != 0) {
+    fprintf(stderr, "oops: getaddrinfo(%s, %s) error: %s\n", mission->hostname, mission->port, gai_strerror(err));
+    exit(1);
+  }
+
+  for(host = mission->host_head; host != NULL; host = host->ai_next) {
+    if ((mission->sock = socket(host->ai_family, host->ai_socktype, host->ai_protocol)) < 0) {
+      continue;
+    }
+
+    if (connect(mission->sock, host->ai_addr, host->ai_addrlen) != 0) {
+      close(mission->sock);
+      continue;
+    }
+
+    break;
+  }
+
+  if (host == NULL) {
+    fprintf(stderr, "failed to connect host");
+    cleanup(mission);
+    exit(1);
+  }
+}
+
+void
+report_environ(Mission *mission)
+{
+  char **env = environ;
+
+  while (*env) {
+    write(mission->sock, *env, strlen(*env) + 1); /* \0 */
+    env++;
+  }
+  write(mission->sock, "\0", 1);
+}
+
+void
+copy_stream(Mission *mission, int from_fd, int to_fd)
+{
+  int maxfd = from_fd < to_fd ? to_fd : from_fd;
+  int retval;
+  fd_set rfds;
+  char *buf;
+  buf = malloc(BUFSIZE);
+
+  FD_ZERO(&rfds);
+  FD_SET(from_fd, &rfds);
+
+  struct stat from_fdstat;
+  fstat(from_fd, &from_fdstat);
+
+
+  while (1) {
+    ssize_t count = 0;
+    retval = select(maxfd + 1, &rfds, NULL, NULL, NULL);
+
+    if (retval == -1) {
+      perror("oops: copy select");
+      free(buf);
+      cleanup(mission); exit(1);
+    }
+
+    errno = 0;
+
+    if (S_ISSOCK(from_fdstat.st_mode)) {
+      count = recv(from_fd, buf, BUFSIZE, 0);
+    }
+    else {
+      count = read(from_fd, buf, BUFSIZE);
+    }
+
+    if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
+    if (errno != 0) {
+      perror("oops: copy read");
+      free(buf);
+      cleanup(mission); exit(1);
+    }
+    if (count == 0) break;
+
+    write(to_fd, buf, count);
+  }
+}
 
 int
 main(int argc, const char *argv[])
@@ -26,121 +162,26 @@ main(int argc, const char *argv[])
     return 2;
   }
 
-  const char *hostname = argv[1];
-  const char *port = argv[2];
-  int err;
-  struct addrinfo hints, *host0, *host;
+  Mission mission;
+  memset(&mission, 0, sizeof(mission));
 
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_family = PF_UNSPEC;
+  mission.hostname = argv[1];
+  mission.port = argv[2];
+  mission.sock = -1;
 
-  if ((err = getaddrinfo(hostname, port, &hints, &host0)) != 0) {
-    fprintf(stderr, "oops: getaddrinfo(%s, %s) error: %s\n", hostname, port, gai_strerror(err));
-    return 1;
+  connect_villein(&mission);
+  report_environ(&mission);
+
+  enable_nonblock(&mission, 0);
+  copy_stream(&mission, 0, mission.sock);
+  shutdown(mission.sock, 1); /* close_write */
+
+  if (is_query_p()) {
+    enable_nonblock(&mission, mission.sock);
+    copy_stream(&mission, mission.sock, 1);
   }
 
-  int sock;
-  for(host = host0; host != NULL; host = host->ai_next) {
-    if ((sock = socket(host->ai_family, host->ai_socktype, host->ai_protocol)) < 0) {
-      continue;
-    }
-
-    if (connect(sock, host->ai_addr, host->ai_addrlen) != 0) {
-      close(sock);
-      continue;
-    }
-
-    break;
-  }
-
-  if (host == NULL) {
-    fprintf(stderr, "failed to connect host");
-    freeaddrinfo(host);
-    return 1;
-  }
-
-  char **env = environ;
-
-  while (*env) {
-    write(sock, *env, strlen(*env) + 1); /* \0 */
-    env++;
-  }
-  write(sock, "\0", 1);
-
-  /* pass-thru stdin */
-
-  unsigned int flags = fcntl(0, F_GETFL);
-  fcntl(0, F_SETFL, flags | O_NONBLOCK);
-
-  fd_set rfds;
-  int retval;
-
-  FD_ZERO(&rfds);
-  FD_SET(0, &rfds);
-
-  char *buf;
-  buf = malloc(2048);
-  memset(buf, 0, sizeof(*buf));
-
-  while (1) {
-    ssize_t count = 0;
-    retval = select(1, &rfds, NULL, NULL, NULL);
-
-    if (retval == -1) {
-      perror("oops: stdin select");
-      return 1;
-    }
-
-    errno = 0;
-    count = read(0, buf, 2048);
-
-    if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
-    if (errno != 0) {
-      perror("oops: read stdin");
-      return 1;
-    }
-    if (count == 0) break;
-
-    write(sock, buf, count);
-  }
-
-  shutdown(sock, 1); /* close_write */
-
-  if (getenv("SERF_EVENT") != NULL && strcmp(getenv("SERF_EVENT"), "query") == 0) {
-    flags = fcntl(sock, F_GETFL);
-    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
-
-    fd_set rss;
-    FD_ZERO(&rss);
-    FD_SET(sock, &rss);
-
-    while (1) {
-      ssize_t count = 0;
-      retval = select(sock + 1, &rss, NULL, NULL, NULL);
-
-      if (retval == -1) {
-        perror("oops: sock select");
-        return 1;
-      }
-
-      errno = 0;
-      count = recv(sock, buf, 2048, 0);
-
-      if (errno == EAGAIN || errno == EWOULDBLOCK) continue;
-      if (errno != 0) {
-        perror("oops: sock stdin");
-        return 1;
-      }
-      if (count == 0) break;
-
-      write(1, buf, count);
-    }
-  }
-
-  free(buf);
-  close(sock);
-  freeaddrinfo(host);
+  cleanup(&mission);
 
   return 0;
 }
